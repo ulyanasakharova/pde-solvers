811e4ad649ef7b4783427fefc172416762d184a3
Ulyana Sakharova <u.a.saharova@mail.ru>
2024-11-17 20:55:28 +0300

    initial commit

---
+++ Block1/Block1/charts.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+
+rawData = pd.read_csv('res.csv', encoding='windows-1251')
+parametersNames = rawData.columns.tolist()[2:]
+[timeLabel, coordLabel] = rawData.columns.tolist()[:2]
+plotsCount = len(parametersNames)
+
+fig = plt.figure()
+axes = [plt.subplot(plotsCount, 1, _ + 1) for _ in range(plotsCount)]
+
+data_skip = len(set(rawData[coordLabel]))
+
+def init_func():
+    for i in range(len(axes)):
+        axes[i].clear()
+        axes[i].grid(visible=True)
+        axes[i].set_xlabel(coordLabel)
+        axes[i].set_ylabel(parametersNames[i])
+        coordData = rawData[coordLabel]
+        paramData = rawData[parametersNames[i]]
+        xLim = [min(coordData) - 0.1 * (max(coordData) - min(coordData)), max(coordData) + 0.1 * (max(coordData) - min(coordData))]
+        yLim = [min(paramData) - 0.1 * (max(paramData) - min(paramData)), max(paramData) + 0.1 * (max(paramData) - min(paramData))]
+        axes[i].set_xlim(xLim)
+        axes[i].set_ylim(yLim)
+
+
+def update_plot(i):
+    init_func()
+    for j in range(len(axes)):
+        coordData = rawData[coordLabel]
+        paramData = rawData[parametersNames[j]]
+        axes[j].plot(coordData[i * data_skip: (i + 1) * data_skip], paramData[i * data_skip: (i + 1) * data_skip], color='b')
+
+anim = FuncAnimation(fig,
+                     update_plot,
+                     frames=len(set(rawData[timeLabel])),
+                     init_func=init_func,
+                     interval=100, repeat=False)
+
+plt.show()
---
+++ Blok=ck11/Blok=ck11/Blok=ck11.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    double length_pipeline = 100;
+    double step_of_grid = 5;
+    double r0_a = 850;
+    double r0_b = 860;
+    vector<double> r0_0(step_of_grid);
+    vector<double> r0_1(step_of_grid);
+
+    for (int i = 0; i <= (step_of_grid - 1); i++)
+    {
+        r0_0[i] = r0_a;
+    }
+
+    for (int i = 0; i <= (step_of_grid - 1); i++)
+    {
+        if (i == 0)
+        {
+            r0_1[i] = r0_b;
+        }
+        else
+        {
+            r0_1[i] = r0_0[i - 1];
+        }
+    }
+
+    cout << r0_0[0] << " " << r0_0[1] << " " << r0_0[2] << " " << r0_0[3] << " " << r0_0[4] << endl;
+    cout << r0_1[0] << " " << r0_1[1] << " " << r0_1[2] << " " << r0_1[3] << " " << r0_1[4] << endl;
+
+    return 0;
+}
